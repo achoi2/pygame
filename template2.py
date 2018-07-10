b056b159fe5fa0bc3b6798d4e7f008bff8c4ce81
Andrew Choi <andrewchoi1@hotmail.com>
2018-07-10 17:16:30 -0400

    added player movement

--- template2.py
+++ template2.py
@@ -17,22 +17,18 @@ def main():
             self.image = pygame.image.load('images/dog.png')
             self.image = pygame.transform.scale(self.image, (64, 64))
             self.rect = self.image.get_rect()
-            self.rect.center = (width / 2, height / 2)
-            self.speedx = 10
-            self.speedy = 10
+            self.rect.centerx = width / 2
+            self.rect.bottom = height
+            self.speedx = 0
 
         def update(self):
             self.speedx = 0
-            self.speedy = 0
             keystate = pygame.key.get_pressed()   
             if keystate[pygame.K_LEFT] and self.rect.x > self.speedx:
                 self.rect.x -= 5
             if keystate[pygame.K_RIGHT] and self.rect.x < width - 64 - self.speedx:
                 self.rect.x += 5
-            if keystate[pygame.K_UP] and self.rect.y > self.speedy:
-                self.rect.y -= 5
-            if keystate[pygame.K_DOWN] and self.rect.y < height - 64 - self.speedy: 
-                self.rect.y += 5
+            
 
     class Mob(pygame.sprite.Sprite):
         def __init__(self):
@@ -41,26 +37,17 @@ def main():
             self.image = pygame.transform.scale(self.image, (64, 64))
             self.rect = self.image.get_rect()
             self.rect.x = random.randrange(width - self.rect.width)
-            self.rect.y = random.randrange(-100, -40)
-            self.speedy = random.randrange(1, 8)
-            self.speedx = random.randrange(1, 8)
+            self.rect.y = random.randrange(-100, -10)
+            self.speedy = random.randrange(2, 8)
+            
 
         def update(self):          
             self.rect.y += self.speedy
-            self.rect.x += self.speedx
 
-            if self.rect.x > width -64:
-                self.speedx = -random.randrange(1, 8)
-            if self.rect.y > height -64:
-                self.speedy = -random.randrange(1, 8)
-            if self.rect.x < 0:
-                self.speedx = random.randrange(1, 8)
-            if self.rect.y < 0:
-                self.speedy = random.randrange(1, 8) 
-            # if self.rect.top > height + 10:
-            #     self.rect.x = random.randrange(width - self.rect.width)
-            #     self.rect.y = random.randrange(-100, -40)
-            #     self.speedy = random.randrange(1, 8)
+            if self.rect.top > height + 10:
+                self.rect.x = random.randrange(width - self.rect.width)
+                self.rect.y = random.randrange(-100, -40)
+                self.speedy = random.randrange(1, 8)
         
                                 
     # initialize and create window 
@@ -93,6 +80,12 @@ def main():
 
         # update
         all_sprites.update()
+        
+        #collision
+        hits = pygame.sprite.spritecollide(hero, mobs, False)
+        if hits:
+            running = False
+
         # draw / render
         screen.fill(blue_color)
         screen.blit(background, background_rect)
